144b6b297da8f6114d45506642e3dbb35adbd384
Zlaten-eng <ronojepsat70@gmail.com>
2025-06-02 19:49:39 +0300

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -20,3 +20,33 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         reply(`${e}`)
     }
 })
+// musicPlayer.js
+
+// Step 1: Add your music links here
+const musicLinks = [
+  "https://files.catbox.moe/xci982.mp3",
+    "https://files.catbox.moe/utbujd.mp3",
+    "https://files.catbox.moe/w2j17k.m4a",
+    "https://files.catbox.moe/851skv.m4a",
+    "https://files.catbox.moe/qnhtbu.m4a",
+    "https://files.catbox.moe/lb0x7w.mp3",
+    "https://files.catbox.moe/efmcxm.mp3",
+    "https://files.catbox.moe/gco5bq.mp3",
+    "https://files.catbox.moe/26oeeh.mp3",
+    "https://files.catbox.moe/a1sh4u.mp3",
+    "https://files.catbox.moe/vuuvwn.m4a",
+    "https://files.catbox.moe/wx8q6h.mp3",
+    "https://files.catbox.moe/uj8fps.m4a",
+  // Add as many links as you want
+];
+
+// Step 2: Function to pick a random music link
+function getRandomMusicLink() {
+  const index = Math.floor(Math.random() * musicLinks.length);
+  return musicLinks[index];
+}
+
+// Step 3: Export the link (for bot use)
+module.exports = {
+  getRandomMusicLink,
+};
