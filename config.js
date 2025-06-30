8edf86f2bc00b7884bb76cd88625228802179235
Conway-eng <Mornah188@gmail.com>
2025-06-30 18:08:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,83 +1,44 @@
-              // POWERED BY HACKLINK TECH.INC
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+// POWERED BY HACKLINK TECH.INC
+require('dotenv').config();
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
+const convertToBool = (text) => text === 'true';
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "pur your session id here ",
-// add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY ROVER-XMD*",
-// set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/82b8gr.jpg",
-// add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "V",
-// add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "ROVER-XMD",
-// add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "ROVER-XMD",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,😭,💙,👎,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254769677305",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "CONWAY",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© HACKLINK TECH.INC",
-// add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/82b8gr.jpg",
-// add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> ROVER-XMD IS ALIVE SUCKER!!!💯",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: process.env.MODE || "private",
-// make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-// make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-// maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
-// make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "254769677305",
-//replace with your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
-// change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
-// make it true for auto recoding 
+  SESSION_ID: process.env.SESSION_ID,
+  AUTO_STATUS_SEEN: convertToBool(process.env.AUTO_STATUS_SEEN),
+  AUTO_STATUS_REPLY: convertToBool(process.env.AUTO_STATUS_REPLY),
+  AUTO_STATUS_REACT: convertToBool(process.env.AUTO_STATUS_REACT),
+  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG,
+  WELCOME: convertToBool(process.env.WELCOME),
+  ADMIN_EVENTS: convertToBool(process.env.ADMIN_EVENTS),
+  ANTI_LINK: convertToBool(process.env.ANTI_LINK),
+  MENTION_REPLY: convertToBool(process.env.MENTION_REPLY),
+  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL,
+  PREFIX: process.env.PREFIX,
+  BOT_NAME: process.env.BOT_NAME,
+  STICKER_NAME: process.env.STICKER_NAME,
+  CUSTOM_REACT: convertToBool(process.env.CUSTOM_REACT),
+  CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS,
+  DELETE_LINKS: convertToBool(process.env.DELETE_LINKS),
+  OWNER_NUMBER: process.env.OWNER_NUMBER,
+  OWNER_NAME: process.env.OWNER_NAME,
+  DESCRIPTION: process.env.DESCRIPTION,
+  ALIVE_IMG: process.env.ALIVE_IMG,
+  LIVE_MSG: process.env.LIVE_MSG,
+  READ_MESSAGE: convertToBool(process.env.READ_MESSAGE),
+  AUTO_REACT: convertToBool(process.env.AUTO_REACT),
+  ANTI_BAD: convertToBool(process.env.ANTI_BAD),
+  MODE: process.env.MODE,
+  ANTI_LINK_KICK: convertToBool(process.env.ANTI_LINK_KICK),
+  AUTO_VOICE: convertToBool(process.env.AUTO_VOICE),
+  AUTO_STICKER: convertToBool(process.env.AUTO_STICKER),
+  AUTO_REPLY: convertToBool(process.env.AUTO_REPLY),
+  ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE),
+  PUBLIC_MODE: convertToBool(process.env.PUBLIC_MODE),
+  AUTO_TYPING: convertToBool(process.env.AUTO_TYPING),
+  READ_CMD: convertToBool(process.env.READ_CMD),
+  DEV: process.env.DEV,
+  ANTI_VV: convertToBool(process.env.ANTI_VV),
+  ANTI_DEL_PATH: process.env.ANTI_DEL_PATH,
+  AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING)
 };
